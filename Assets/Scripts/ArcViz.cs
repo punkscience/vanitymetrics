cc9831f5bee6153130ab20313e49cb6041d60851
Chris Cunningham <chris@orpheusinteractive.com>
2016-01-29 19:39:09 -0400

    Base ArcViz script, currently works in scene only

---
+++ Assets/Scripts/ArcViz.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class ArcViz : MonoBehaviour {
+    private LineRenderer _renderer;
+
+    [Range(0f, 100f)]
+    [SerializeField] private float _power = 30f;
+    [Range(0.01f, 1f)]
+    [SerializeField] private float _timeStep = 0.5f;
+    [SerializeField] private float _maxTime = 10f;
+
+    private void Awake() {
+        _renderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update() {
+        PlotTrajectory(Vector3.zero, transform.forward * _power, _timeStep, _maxTime);
+    }
+
+    public Vector3 PlotTrajectoryAtTime(Vector3 start, Vector3 startVelocity, float time) {
+        return start + startVelocity * time + Physics.gravity * time * time * 0.5f;
+    }
+
+    public void PlotTrajectory(Vector3 start, Vector3 StartVelocity, float timeStep, float maxTime) {
+        var prev = start;
+        for (int i = 1;; i++) {
+            float t = timeStep * i;
+            if (t > maxTime) break;
+
+            var pos = PlotTrajectoryAtTime(start, StartVelocity, t);
+            if (Physics.Linecast(prev, pos)) break;
+            Debug.DrawLine(prev, pos, Color.red);
+            prev = pos;
+        }
+    }
+}
