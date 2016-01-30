b04a21df38ccc98aef0dadba2a9f321ceb2d8aec
Chris Cunningham <chris@orpheusinteractive.com>
2016-01-30 12:11:08 -0400

    projectile script

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+    [SerializeField] private float _killY = 0f;
+    private void Update() {
+        if (transform.position.y <= _killY) {
+            Destroy(gameObject);
+        }
+    }
+}
