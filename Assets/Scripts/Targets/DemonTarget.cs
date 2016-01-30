3b827bf1fa7cdf03951207e7c2d038e7aeff0632
Vincent Fellows <vincentfellows@gmail.com>
2016-01-30 18:48:29 -0400

    Demons get stunned when they get hit.

--- Assets/Scripts/Targets/DemonTarget.cs
+++ Assets/Scripts/Targets/DemonTarget.cs
@@ -12,6 +12,8 @@ public class DemonTarget : MonoBehaviour {
 	// demon statistics
 	protected float demonHealth;
 	protected float demonSpeed;
+	protected Vector3 stunPos;
+	protected float stunTimer = 0;
 
 	// demon positioning
 	protected Vector3 demonStartPoint;
@@ -50,24 +52,47 @@ public class DemonTarget : MonoBehaviour {
 		demonHealth -= damageValue;
 
 		if (demonHealth <= 0.0F) {
+			// demon is dead
 			deathTimer = 1.0F;
 
 			// disable the collider so arrows don't collide with it anymore
 			gameObject.GetComponent<Collider> ().enabled = false;
+
+		} else {
+
+			// stun the demon
+			stunTimer = 1.5F;
+			stunPos = transform.position;
 		}
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
-		if (deathTimer == 0.0F) {
-			transform.position += transform.forward * (demonSpeed * Time.deltaTime);
-		} else {
+
+		if (stunTimer > 0) {
+			stunTimer -= Time.deltaTime;
+			if (stunTimer > 1.0F) {
+				transform.position = (stunPos + Random.insideUnitSphere * 0.1F);
+			} else {
+				transform.position = stunPos;
+			}
+
+		} else if (deathTimer > 0.0F) {
+
+			// death timer
 			deathTimer -= Time.deltaTime;
+			Vector3 deathPos = (Random.insideUnitSphere * 0.2F);
+			deathPos.y += 2.0F * Time.deltaTime;
+			transform.position -= deathPos;
 
 			if (deathTimer <= 0.0F) {
 				Destroy (gameObject);
 			}
+		} else {
+			
+			// move the demon forward
+			transform.position += transform.forward * (demonSpeed * Time.deltaTime);
 		}
 	}
 }
