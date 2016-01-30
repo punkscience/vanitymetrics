39b35595d77476cd517d0a91562f3014cd6fde01
Chris Cunningham <chris@orpheusinteractive.com>
2016-01-30 12:46:33 -0400

    added arrow

--- Assets/Scripts/ProjectileLauncher.cs
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -4,7 +4,7 @@ public class ProjectileLauncher : MonoBehaviour {
     [SerializeField] private GameObject _projectile;
 
     public void Fire(float force) {
-        var shot = (GameObject) Instantiate(_projectile, transform.position, Quaternion.identity);
+        var shot = (GameObject) Instantiate(_projectile, transform.position, transform.rotation);
         shot.GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
