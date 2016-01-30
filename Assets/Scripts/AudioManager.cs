a25ea037a2ed7cdc7e0d27d78968551a0cd1644f
Darryl Wright <darryl@orpheusinteractive.com>
2016-01-30 09:56:03 -0400

    Beginning work on audio.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	[SerializeField]
+	private AudioSource feMusic;
+
+	[SerializeField]
+	private AudioSource ambience;
+
+	public void PlayMainMenuMusic() {
+		feMusic.Play();
+	}
+
+
+	public void StopMainMenuMusic() { 
+		feMusic.Stop();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
