28706b596fdc1f27cfe5ac2a6cef76d96efc7209
Darryl Wright <darryl@orpheusinteractive.com>
2016-01-30 14:21:14 -0400

    Added Wake.

---
+++ Assets/Scripts/Targets/Wake.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wake : MonoBehaviour {
+
+	private GameObject froth;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
