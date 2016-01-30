e98ad38647ceb4c06f9bc4cb116ccb934ed604a3
Vincent Fellows <vincentfellows@gmail.com>
2016-01-29 20:06:42 -0400

    Boat Code started

--- Assets/Scripts/Targets/BoatTarget.cs
+++ Assets/Scripts/Targets/BoatTarget.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class BoatTarget : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	protected float boatSpeed = 0;
+
+	void Init (float angle, float speed) {
+		transform.localPosition = 0;
+		transform.eulerAngles = new Vector3 (0, angle, 0);
+		boatSpeed = speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
