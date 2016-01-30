10b085a113bb9cf16807e3e9fb06924a9697837f
Vincent Fellows <vincentfellows@gmail.com>
2016-01-30 15:13:33 -0400

    Demon script started. Also boat parent object renamed to Boats Boats Boats for clarity.

---
+++ Assets/Scripts/Targets/DemonTarget.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemonTarget : MonoBehaviour {
+
+	protected float demonHealth;
+	protected float demonSpeed;
+	protected Transform demonTarget;
+
+	public void Init (float angle, float health, float speed, Transform startTransform, Transform target) {
+
+		transform.position = startTransform.position;
+		transform.localEulerAngles = new Vector3 (0, angle + 180.0F, 0);
+
+		demonHealth = health;
+		demonSpeed = speed;
+		demonTarget = target;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		transform.position 
+	}
+}
