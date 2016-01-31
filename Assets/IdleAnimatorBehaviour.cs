bf8961c68d7438206c3de9a71c9e03d0fd2570ba
Chris Cunningham <chris@orpheusinteractive.com>
2016-01-30 20:49:32 -0400

    move bow offscreen stuff

---
+++ Assets/IdleAnimatorBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleAnimatorBehaviour : StateMachineBehaviour {
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	    var mover = animator.GetComponent<BowMover>();
+        mover.MoveOffScreen();
+	}
+}
