cb35f762c2c1c66a9e543ec25d145e33eb612ce0
MarkPod <mark.pod@hotmail.com>
2016-01-31 10:36:32 -0400

    Rain maker and stuff

---
+++ Assets/Scripts/RainMaking.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainMaking : MonoBehaviour {
+
+    
+    float heaviness = 5;
+    [SerializeField]
+    GameObject drop;
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < heaviness; i++)
+        {
+            Instantiate(drop);
+        }
+
+	}
+}
