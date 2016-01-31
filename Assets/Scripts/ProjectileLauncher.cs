1b2fd8475d8e483ae51fab8855f4e06f2eacfe48
Darryl Wright <darryl@orpheusinteractive.com>
2016-01-30 20:44:46 -0400

    Arrow sound.

--- Assets/Scripts/ProjectileLauncher.cs
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -4,6 +4,9 @@ public class ProjectileLauncher : MonoBehaviour {
     [SerializeField] private GameObject _projectile;
 
     public void Fire(float force) {
+		AudioSource fire = GetComponent<AudioSource> ();
+		fire.Play ();
+
         var shot = (GameObject) Instantiate(_projectile, transform.position, transform.rotation);
         shot.GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Impulse);
     }
