4529681aabe7ca3e5897a93a4a7fc036443b6ab0
Vincent Fellows <vincentfellows@gmail.com>
2016-02-05 00:09:13 -0400

    Modified the skip code so that it now only skips a panel. Also a new smaller icon was added.

--- Assets/Scripts/TitleCards.cs
+++ Assets/Scripts/TitleCards.cs
@@ -11,6 +11,8 @@ public class TitleCards : MonoBehaviour {
 	public uint[] cardDelays;
 	public AudioSource[] sources;
 	private uint currentTitleCard = 0;
+	private bool fadeOut;
+	private bool canceledFade;
 
 	// Use this for initialization
 	void Start () {
@@ -23,8 +25,10 @@ public class TitleCards : MonoBehaviour {
 
 
 	void DoCards() {
-		var tween = TweenAlpha.Tween (titleCards[currentTitleCard], 2.0f, 1f, DisplayCard );
-		tween.delay = startDelay;
+		StartTween (false);
+
+//		var tween = TweenAlpha.Tween (titleCards[currentTitleCard], 2.0f, 1f, DisplayCard );
+//		tween.delay = startDelay;
 	}
 
 
@@ -36,22 +40,97 @@ public class TitleCards : MonoBehaviour {
 		}
 
 		// Fade out this card after the delay
-		var tween = TweenAlpha.Tween (titleCards[currentTitleCard], 2.0f, 0f, NextCard );
-		tween.delay = cardDelays[currentTitleCard];
+
+		StartTween (true);
+//		var tween = TweenAlpha.Tween (titleCards[currentTitleCard], 2.0f, 0f, NextCard );
+//		tween.delay = cardDelays[currentTitleCard];
 	}
 
-	void NextCard() {
-		if (currentTitleCard+1 == titleCards.Length) {
+	void StartTween (bool fadeOut) {
+		
+		float delay = cardDelays [currentTitleCard];
+		Graphic img = titleCards [currentTitleCard].GetComponent<Graphic> ();
+
+		if (fadeOut) {
+			img.color = new Color (1, 1, 1, 1.0F);
+		} else {
+			img.color = new Color (1, 1, 1, 0.0F);
+		}
+
+		StartCoroutine (Tween (img, delay, fadeOut));
+	}
+
+	IEnumerator Tween (Graphic img, float delay, bool fadeOut) {
+
+		// increment the delay timer
+		float timer = 0.0F;
+		bool cancelFadeIn = false;
+
+		// wait for the delay
+		while (timer < delay) {
+			timer += Time.deltaTime;
+			if (canceledFade) {
+
+				// if the user cancelled during the fade in only cancel the fade in
+				if (!fadeOut) {
+					img.color = new Color (1, 1, 1, 1.0F);
+					cancelFadeIn = true;
+					canceledFade = false;
+				}
+				break;
+			}
+			yield return null;
+		}
+
+		if (!cancelFadeIn) {
+			timer = 0.0F;
+			while (timer < 1.0F) {
+				img.color = new Color (1, 1, 1, (fadeOut ? (1.0F - timer) : timer));
+
+				if (canceledFade) {
+					break;
+				}
+
+				timer += Time.deltaTime / 2.0F;
+				yield return null;
+			}
+		}
+
+		// if the player canceled skip to the next card
+		if (canceledFade) {
+			img.color = new Color (1, 1, 1, 0.0F);
+			canceledFade = false;
+			IncrementCard ();
+			DisplayCard ();
+		} else if (fadeOut) {
+			NextCard ();
+		} else {
+			DisplayCard ();
+		}
+	}
+
+	void IncrementCard () {
+		if (currentTitleCard + 1 == titleCards.Length) {
 			SceneManager.LoadScene ("FrontEnd", LoadSceneMode.Single);
 			return; // We're done.
 		}
 
-		var tween = TweenAlpha.Tween( titleCards[++currentTitleCard], 2.0f, 1f, DisplayCard );
-		tween.delay = 2.0f;
+		currentTitleCard++;
+	}
+
+	void NextCard() {
+		IncrementCard ();
+		StartTween (false);
+//		var tween = TweenAlpha.Tween( titleCards[++currentTitleCard], 2.0f, 1f, DisplayCard );
+//		tween.delay = 2.0f;
 	}
 
 	public void SkipCards() {
-		SceneManager.LoadScene ("FrontEnd", LoadSceneMode.Single);
+		if (sources [currentTitleCard] != null) {
+			sources [currentTitleCard].Stop ();
+		}
+		canceledFade = true;
+//		SceneManager.LoadScene ("FrontEnd", LoadSceneMode.Single);
 	}
 
 
