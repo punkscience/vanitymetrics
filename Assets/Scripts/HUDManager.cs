e65c3ae20dca263dc8bc64ad542fd68fee811f1f
Chris Cunningham <chris@rightsomegoodgames.ca>
2016-02-06 12:45:18 -0400

    added the new settings menu and the invertX and Y options, need to hook it up to the input manager next

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -1,29 +1,66 @@
-﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class HUDManager : MonoBehaviour {
 
 	public static bool isPaused = false;
-	public static bool invertXAxis = false;
+	public static bool invertXAxis;
+	public static bool invertYAxis;
+
+    [SerializeField] private GameObject _pauseCG;
+    [SerializeField] private GameObject _settingsCG;
+
+    [SerializeField] private Toggle _xToggle;
+    [SerializeField] private ParticleSystem _xToggleFire;
+    [SerializeField] private Toggle _yToggle;
+    [SerializeField] private ParticleSystem _yToggleFire;
 
 	private GameObject pauseMenu;
 	private GameObject HUD;
 	private GameObject youWin;
 
-	// Use this for initialization
 	void Start () {
-		
 		pauseMenu = GameObject.Find ("panel_PauseMenu");
 		HUD = GameObject.Find ("panel_HUD");
-
 		if (pauseMenu.activeSelf) { pauseMenu.SetActive (false); }
+
+	    invertXAxis = SettingsInvertX;
+	    invertYAxis = SettingsInvertY;
+	    _xToggle.isOn = SettingsInvertX;
+	    _yToggle.isOn = SettingsInvertY;
+
+        if (_xToggle.isOn) _xToggleFire.Play(true);
+        else _xToggleFire.Play(true);
+
+        if (_yToggle.isOn) _yToggleFire.Play(true);
+        else _yToggleFire.Play(true);
+
+
 	}
 
 	public void InverseXAxis () {
 		invertXAxis = !invertXAxis;
 	}
+
+    public void InvertXAxis(bool val) {
+        SettingsInvertX = val;
+        invertXAxis = val;
+        if (val)
+            _xToggleFire.Play(true);
+        else 
+            _xToggleFire.Stop(true);
+    }
+
+    public void InvertYAxis(bool val) {
+        SettingsInvertY = val;
+        invertYAxis = val;
+        if (val)
+            _yToggleFire.Play(true);
+        else 
+            _yToggleFire.Stop(true);
+    }
 
 	public void SetPause(bool isPaused) {
 		if (isPaused) {
@@ -37,6 +74,19 @@ public class HUDManager : MonoBehaviour {
 			HUD.SetActive (true);
 		}
 	}
+
+    public void ToSettingsMenu() {
+        TweenCGAlpha.Tween(_pauseCG, 0.3f, 1f, 0f, () => { _pauseCG.SetActive(false); });
+        _settingsCG.SetActive(true);
+        TweenCGAlpha.Tween(_settingsCG, 0.3f, 0f, 1f);
+    }
+
+    public void ToPauseMenu() {
+        TweenCGAlpha.Tween(_settingsCG, 0.3f, 1f, 0f, () => { _settingsCG.SetActive(false); });
+        _pauseCG.SetActive(true);
+        TweenCGAlpha.Tween(_pauseCG, 0.3f, 0f, 1f);
+        
+    }
 
 	public void ReturnToMain() {
 		isPaused = false;
@@ -55,4 +105,14 @@ public class HUDManager : MonoBehaviour {
 		Time.timeScale = 1f;
 		SceneManager.LoadScene("Main");
 	}
+
+    private bool SettingsInvertX {
+        get { return PlayerPrefs.GetInt("SettingsInvertX", 0) == 1; }
+        set { PlayerPrefs.SetInt("SettingsInvertX", value ? 1 : 0); }
+    }
+
+    private bool SettingsInvertY {
+        get { return PlayerPrefs.GetInt("SettingsInvertY", 0) == 1; }
+        set { PlayerPrefs.SetInt("SettingsInvertY", value ? 1 : 0); }
+    }
 }
---
+++ Assets/Scripts/UnScaledTimeParticle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UnScaledTimeParticle : MonoBehaviour {
+    [SerializeField] private Toggle _toggle;
+    private ParticleSystem _particles;
+
+    private void Awake() {
+        _particles = GetComponent<ParticleSystem>();
+    }
+
+    private void Update() {
+        _particles.Simulate(Time.unscaledDeltaTime, true, false);
+    }
+
+    private void OnEnable() {
+        if (_toggle.isOn)
+            _particles.Play(true);
+    }
+}
